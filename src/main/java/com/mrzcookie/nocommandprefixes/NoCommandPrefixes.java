0c1905bd3bd6355819d6e038a9aff41a68ae4273
Zachary King <zacharyrking0812@gmail.com>
2022-09-25 10:58:27 -0500

    Optimized Command Removal

--- src/main/java/com/mrzcookie/nocommandprefixes/NoCommandPrefixes.java
+++ src/main/java/com/mrzcookie/nocommandprefixes/NoCommandPrefixes.java
@@ -2,56 +2,77 @@ package com.mrzcookie.nocommandprefixes;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.command.SimpleCommandMap;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandSendEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.SimplePluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public final class NoCommandPrefixes extends JavaPlugin implements Listener {
+    public static FileConfiguration config;
 
     @Override
     public void onEnable() {
         Bukkit.getLogger().info("[NoCommandPrefixes] Enabled NoCommandPrefixes");
-        getConfig().options().copyDefaults(true);
+
+        config = getConfig();
+        config.options().copyDefaults(true);
         saveConfig();
+
         getServer().getPluginManager().registerEvents(this, this);
+
         BukkitScheduler scheduler = Bukkit.getScheduler();
         scheduler.runTaskTimer(this, () -> {
             Bukkit.getLogger().info("[NoCommandPrefixes] Checking for Command Prefixes...");
+
             try {
-                Object result = getPrivateField(Bukkit.getServer().getPluginManager(), "commandMap");
-                SimpleCommandMap commandMap = (SimpleCommandMap) result;
-                Object map = getPrivateField(commandMap, "knownCommands");
-                HashMap<String, Command> knownCommands = (HashMap<String, Command>) map;
+                Constructor<PluginCommand> constructor = PluginCommand.class.getDeclaredConstructor(String.class, Plugin.class);
+                constructor.setAccessible(true);
+
+                Field commandMap = SimplePluginManager.class.getDeclaredField("commandMap");
+                commandMap.setAccessible(true);
+                SimpleCommandMap map = (SimpleCommandMap) commandMap.get(Bukkit.getPluginManager());
+
+                Field knownCommandsField = SimpleCommandMap.class.getDeclaredField("knownCommands");
+                knownCommandsField.setAccessible(true);
+                Map<String, Command> knownCommands = (Map<String, Command>) knownCommandsField.get(map);
+
                 Set<String> commandsRemoved = new HashSet<>();
                 for (String command : knownCommands.keySet()) {
-                    if (getConfig().getBoolean("Disable-All-Prefixes")) {
+                    if (config.getBoolean("Disable-All-Prefixes")) {
                         if (command.contains(":")) {
                             commandsRemoved.add(command);
                         }
                     }
-                    for (String prefix : getConfig().getStringList("Prefixes")) {
+
+                    for (String prefix : config.getStringList("Prefixes")) {
                         if (command.contains(prefix + ":")) {
                             commandsRemoved.add(command);
                         }
                     }
                 }
+
                 Bukkit.getLogger().info("[NoCommandPrefixes] Removing Command Prefixes...");
                 for (String command : commandsRemoved) {
                     knownCommands.remove(command);
                 }
+
                 commandsRemoved.clear();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }, 20L * 5L, 20L * getConfig().getInt("Interval"));
+        }, 20L * 5L, 20L * config.getInt("Interval"));
     }
 
 
@@ -65,12 +86,13 @@ public final class NoCommandPrefixes extends JavaPlugin implements Listener {
         Set<String> commandsRemoved = new HashSet<>();
 
         for (String command : event.getCommands()) {
-            if (getConfig().getBoolean("Disable-All-Prefixes")) {
+            if (config.getBoolean("Disable-All-Prefixes")) {
                 if (command.contains(":")) {
                     commandsRemoved.add(command);
                 }
             }
-            for (String prefix : getConfig().getStringList("Prefixes")) {
+
+            for (String prefix : config.getStringList("Prefixes")) {
                 if (command.contains(prefix + ":")) {
                     commandsRemoved.add(command);
                 }
@@ -78,14 +100,5 @@ public final class NoCommandPrefixes extends JavaPlugin implements Listener {
         }
 
         event.getCommands().removeAll(commandsRemoved);
-    }
-
-    private Object getPrivateField(Object object, String field) throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-        Class<?> clazz = object.getClass();
-        Field objectField = field.equals("commandMap") ? clazz.getDeclaredField(field) : field.equals("knownCommands") ? clazz.getSuperclass().getDeclaredField(field) : clazz.getDeclaredField(field);
-        objectField.setAccessible(true);
-        Object result = objectField.get(object);
-        objectField.setAccessible(false);
-        return result;
     }
 }
